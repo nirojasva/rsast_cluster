b29947ce45d4599232e940ebf78547d10d8c41ac
Nicolas Rojas Varela <nirojasva@unal.edu.co>
2023-04-19 15:39:12 +0200

    updated path sh

--- ExperimentationRSAST/main_cluster.sh
+++ ExperimentationRSAST/main_cluster.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
- 
-#===============================================================================
-# exemples d'options
- 
-#SBATCH --partition=normal    # choix de la partition où soumettre le job
-#SBATCH --time=10:0           # temps max alloué au job (format = m:s ou h:m:s ou j-h:m:s)
-#SBATCH --ntasks=1            # nb de tasks total pour le job
-#SBATCH --cpus-per-task=1     # 1 seul CPU pour une task
-#SBATCH --mem=1000            # mémoire nécessaire (par noeud) en Mo
- 
-#===============================================================================
-#exécution du programme (remplacer exe par le nom du programme
-# ou la ligne de commande à exécuter)
-python "accuracy_per_dataset_rsast (acf_pacf_10000).py"
+#SBATCH --job-name=rsast_project
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=4G
+
+# Activate the Python virtual environment
+source rsast_env/bin/activate
+
+# Execute the Python script
+cd ~/rsast_cluster/ExperimentationRSAST
+python accuracy_per_dataset_rsast.py &
+python 'accuracy_per_dataset_rsast (extra).py' &
+python 'accuracy_per_dataset_rsast (extra_acf_pacf_10000).py' &
+python 'accuracy_per_dataset_rsast (acf_pacf_10000).py' &
+
+wait
+
+echo "All jobs complete"
